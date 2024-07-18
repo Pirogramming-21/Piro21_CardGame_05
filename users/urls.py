673d71372eaf5bff4eb352fbe2481955ac5a2e51
imyyye <daye2382@naver.com>
2024-07-18 22:34:50 +0900

    [fetch] 회원가입&로그인 기능 usrs에 추가

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 
 app_name = 'users'
 urlpatterns = [
+    path('', main, name='main'),
     path('signup', signup, name='signup'),
-    path('login', login, name='login'),
-    path('logout', logout, name='logout')
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import UserCreateForm, LoginForm
 
-
-# Create your views here.
+def main(request):
+    return render(request, 'index.html')
 
 def signup(request):
-    return 0
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('main')  # 메인페이지 URL 연결 수정
+    else:
+        form = UserCreateForm()
+    return render(request, 'signup.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('main')  # 메인페이지 URL 연결 수정
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
-def login(request):
-    return 0
-
-
-def logout(request):
-    return 0
+@login_required
+def logout_view(request):
+    auth_logout(request)
+    return redirect('login')
